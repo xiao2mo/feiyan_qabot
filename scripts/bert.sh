e5570cc763feba5d69c6d7a36cee6cea5801dbf6
houlong <515491847@qq.com>
2020-02-05 16:30:37 +0800

    chore: 调整data位置

--- scripts/bert.sh
+++ scripts/bert.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ZEROMQ_SOCK_TMP_DIR=/tmp/ bert-serving-start -model_dir ./chatbot/chinese_L-12_H-768_A-12 -cpu -num_worker=1
+ZEROMQ_SOCK_TMP_DIR=/tmp/ bert-serving-start -model_dir ./chatbot/bert_pre_models/chinese_wwm_ext_L-12_H-768_A-12 -cpu -num_worker=1
