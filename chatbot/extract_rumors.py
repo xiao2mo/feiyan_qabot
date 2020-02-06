b5f41a3004c00069f7773309e54e302f58d97a07
houlong <515491847@qq.com>
2020-02-06 23:54:04 +0800

    feat: 新增谣言问答

---
+++ chatbot/extract_rumors.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@description: 从数据库中，提取谣言数据，并以rasa nlu的训练数据格式保存。
+"""
+# 添加python包的路径
+import sys
+import os
+PARENT_PATH = os.path.join(os.path.dirname(__file__), '..')
+sys.path.append(PARENT_PATH)
+
+import pymongo
+from utils.constants import MONGO_HOST
+
+MONGO_CLIENT = pymongo.MongoClient(host=MONGO_HOST)
+MONGO_RUMORS = MONGO_CLIENT["2019-nCoV"].DXYRumors
+
+# 路径常量
+NLU_PATH = 'chatbot/data/nlu'
+NLU_FILE = os.path.join(PARENT_PATH, NLU_PATH, 'rumor_nlu.md')
+RESPONSE_FILE = os.path.join(PARENT_PATH, NLU_PATH, 'responses_rumor.md')
+EDA_PATH = os.path.join(os.path.dirname(__file__), 'eda')
+NORMAL_FILE = os.path.join(EDA_PATH, 'rumor.txt')
+AUGMENT_FILE = os.path.join(EDA_PATH, 'rumor_augmented.txt')
+
+# EDA常量
+NUM_AUG = 3 # 一条数据增强次数。
+ALPHA = 0.05 # 替换的百分比
+
+def get_rumors():
+    """ 获取谣言列表
+    @return title(str): 谣言问题
+    @return main_summary(str): 谣言简要回答
+    @return body(str): 回答详情
+    """
+    return [{
+        "title": one["title"],
+        "main_summary": one["mainSummary"].strip().replace("\n",""),
+        "body": one["body"].strip().replace("\n","")
+    } for one in MONGO_RUMORS.find()]
+
+if __name__ == "__main__":
+    rumors = get_rumors()
+    
+    # 写入response文件
+    with open(RESPONSE_FILE, "w") as f:
+        line = ""
+        for i, item in enumerate(rumors):
+            line += "## rumor_{}\n".format(i+1)
+            line += "* faq/rumor_{}\n".format(i+1)
+            line += "\t- {}\\n{}\n".format(item["main_summary"], item["body"])
+            line += "\n"
+        f.write(line)
+    
+    # 写入未增强的语料
+    with open(NORMAL_FILE, 'w') as f:
+        line = ""
+        for i, item in enumerate(rumors):
+            line += "{}\t{}".format(i, item["title"])
+            line += "\n"
+        f.write(line)
+    
+    os.system(f"python {EDA_PATH}/code/augment.py --input {NORMAL_FILE} --output {AUGMENT_FILE} --num_aug={NUM_AUG} --alpha={ALPHA}")
+
+    # 将增强后的语料写成nlu格式
+    content = ""
+    with open(AUGMENT_FILE, 'r') as f:
+        for count, line in enumerate(f.readlines()):
+            line_arr = line.split('\t')
+            if count % (NUM_AUG+1) == 0 and count!= 0:
+                content += "\n"
+            if count % (NUM_AUG+1) == 0:
+                content += "## intent:faq/rumor_{}\n".format(int(line_arr[0])+1)
+            content += "- {}\n".format(line_arr[1].replace(" ", "").replace("\n",""))
+    
+    with open (NLU_FILE, 'w', encoding='utf-8') as f:
+        f.write(content)
--- utils/base_function.py
+++ utils/base_function.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from datetime import datetime
-from utils.constants import STOP_WORDS, FAQ_FILE
+from utils.constants import STOP_WORDS, FAQ_FILE, RUMOR_FILE
 import jieba
 import numpy as np
 from bert_serving.client import BertClient
@@ -17,15 +17,27 @@ def save_voice(content, path):
     with open(voice_file, 'wb') as f:
         f.write(content)
 
+def read_nlu_file(path):
+    """ 读取nlu文件，并返回每个意图的第一个例子
+    """
+    corpus = []
+    flag = 0
+    with open(path, "r") as f:
+        for line in f.readlines():
+            if line.startswith("##"):
+                flag = 1
+            if line.startswith("- ") and flag == 1:
+                corpus.append(line.strip()[2:])
+                flag = 0
+    return corpus
+
 
 def get_questions_list():
     """ 获取faq所有问题
     """
     corpus = []
-    with open(FAQ_FILE, "r") as f:
-        for line in f.readlines():
-            if line.startswith("- "):
-                corpus.append(line.strip()[2:])
+    corpus += read_nlu_file(FAQ_FILE)
+    corpus += read_nlu_file(RUMOR_FILE)
     return corpus
 
 
@@ -34,21 +46,61 @@ def search_related_questions(question, top_k=5):
     @param question(str): 输入的问题
     @param top_k(int): 前k个，默认为5
     """
-    question_encode = BC.encode([question])
-    question_encode_norm = question_encode / np.linalg.norm(question_encode)
-    score_list = np.dot(CORPUS_ENCODING_NORM, question_encode_norm.T)
-    top_k_index = score_list.reshape(-1).argsort()[::-1][:top_k]
-    related_question_str = list(np.array(CORPUS)[top_k_index])
-    return related_question_str
+    vocabulary = set()
+    corpus_tokens = []
+    for sentence in CORPUS:
+        # 分词
+        tokens = jieba.cut(sentence)
+        # 去除停用词
+        tokens_without_stops = [token for token in tokens if token not in STOP_WORDS]
+        corpus_tokens.append(tokens_without_stops)
+        for word in tokens_without_stops:
+            vocabulary.add(word)
+    vocabulary = sorted(list(vocabulary))
+    search_dict = {}
+    for word in vocabulary:
+        search_dict[word] = []
+        for i, tokens in enumerate(corpus_tokens):
+            count = tokens.count(word)
+            if count != 0:
+                search_dict[word].append([i, count])
+    # 进行检索
+    question_tokens = jieba.cut(question)
+    question_tokens_without_stops = [token for token in question_tokens if token not in STOP_WORDS]
+    # 相关的问题有
+    related_question = []
+    for token in question_tokens_without_stops:
+        if token in search_dict.keys():
+            related_question += search_dict[token]
+    # 相关问题匹配到的次数
+    related_question_dict = {}
+    for id_count_pair in related_question:
+        if id_count_pair[0] in related_question_dict:
+            related_question_dict[id_count_pair[0]] += id_count_pair[1]
+        else:
+            related_question_dict[id_count_pair[0]] = id_count_pair[1]
+    # 进行排序
+    sorted_question = sorted(related_question_dict.items(), key=lambda item:item[1], reverse=True)
+    # 输出相似问题排名
+    related_question_str = []
+    for question in sorted_question:
+        related_question_str.append(CORPUS[question[0]])
+    return related_question_str[:top_k]
+    # question_encode = BC.encode([question])
+    # question_encode_norm = question_encode / np.linalg.norm(question_encode)
+    # score_list = np.dot(CORPUS_ENCODING_NORM, question_encode_norm.T)
+    # top_k_index = score_list.reshape(-1).argsort()[::-1][:top_k]
+    # related_question_str = list(np.array(CORPUS)[top_k_index])
+    # return related_question_str
 
 
-print('connecting bert server......')
-BC = BertClient()
+# print('connecting bert server......')
+# BC = BertClient()
 
-print('getting questions list......')
+# print('getting questions list......')
 CORPUS = get_questions_list()
 
-print('encoding questions........')
-CORPUS_ENCODING = BC.encode(CORPUS)
-CORPUS_ENCODING_NORM = CORPUS_ENCODING / \
-    np.linalg.norm(CORPUS_ENCODING, axis=1).reshape(-1, 1)
+# print('encoding questions........')
+# CORPUS_ENCODING = BC.encode(CORPUS)
+# CORPUS_ENCODING_NORM = CORPUS_ENCODING / \
+#     np.linalg.norm(CORPUS_ENCODING, axis=1).reshape(-1, 1)
